b7950a355f47cca886c05be35cbbcbc822611664
Paul Tagliamonte <tag@pault.ag>
2015-08-09 11:19:31 -0400

    Gut the internals to use bindings directly

--- svuorela.go
+++ svuorela.go
@@ -4,18 +4,20 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
-func outputData(w http.ResponseWriter, code int, value *string) {
+func outputData(w http.ResponseWriter, code int, value []byte) {
 	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 	w.WriteHeader(code)
 	if value != nil {
-		w.Write([]byte(*value))
+		w.Write(value)
 	}
 }
 
 func main() {
-	db, err := LoadDatabase("svuorela.db")
+	db, err := leveldb.OpenFile("svuorela.db", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -25,14 +27,22 @@ func main() {
 		log.Printf("%s\n", req.Method)
 		switch req.Method {
 		case "GET":
-			outputData(w, 200, db.Get(req.URL.Path))
+			data, _ := db.Get([]byte(req.URL.Path), nil)
+			// err is set when the key's not found
+			// if err != nil {
+			// 	log.Printf("%s\n", err)
+			// 	outputData(w, 500, nil)
+			// 	return
+			// }
+			outputData(w, 200, data)
 		case "POST":
 			data, err := ioutil.ReadAll(req.Body)
 			if err != nil {
 				log.Printf("%s\n", err)
 				outputData(w, 500, nil)
+				return
 			}
-			db.Write(req.URL.Path, string(data))
+			db.Put([]byte(req.URL.Path), []byte(data), nil)
 		default:
 			return
 		}
